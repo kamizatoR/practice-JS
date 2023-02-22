33cdc6b691908baa2bed02f8d7237d79b387303a
KR <112645912+kamizatoR@users.noreply.github.com>
2023-02-22 13:32:31 +0000

    practice jslet while for

---
+++ app.js
@@ -0,0 +1,52 @@
+// alert('jsファイルから記述してます')
+
+// let test = "変数テスト";
+
+// alert(test)
+
+// let calc = 1 + 4
+
+// let string = "文字列" + "を連結してます"
+
+// alert(calc)
+
+// alert(string + calc)
+
+// let orange = 1000;
+// let apple = 1000;
+
+// if (orange < apple){
+//   alert("orangeよりappleのほうが高いです");
+// } else if(orange == apple) {
+//   alert("orangeとappleは同じ料金です");
+// } else {
+//   alert("appleよりorangeのほうが高いです");
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count += 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// do {
+//   num = num * 2;
+//   count += 1;
+
+// } while (num > max);
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
