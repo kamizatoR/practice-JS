5f980877331149c9bcb38d9afba3d934312cf8df
KR <112645912+kamizatoR@users.noreply.github.com>
2023-02-27 21:11:06 +0000

    practice var let const

--- app.js
+++ app.js
@@ -42,11 +42,89 @@
 
 // alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "giro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "ichiro"
+// console.log(nickname)
+
+// var str = "webcamp" //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+// console.log(i)
+
+// function foo() {
+//   var x = "webcamp"
+//   {
+//     var y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+foo()
