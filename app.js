e7899b9a5814c51ce41f258e15f47cb461f65f81
KR <112645912+kamizatoR@users.noreply.github.com>
2023-02-28 05:24:25 +0000

    practice method

--- app.js
+++ app.js
@@ -119,12 +119,111 @@
 
 // foo()
 
-var str = "webcamp"
+// var str = "webcamp"
 
-function foo() {
-  console.log(str)
-  var str = "dmm webcamp"
-  console.log(str)
+// function foo() {
+//   console.log(str)
+//   var str = "dmm webcamp"
+//   console.log(str)
+// }
+
+// foo()
+
+// let alertString;
+
+// alertString = addString("WebCamp");
+
+// alert(addString("Dmm"));
+
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+
+
+while ((user_hand != 'グー') && (user_hand != 'チョキ') && (user_hand != 'パー') && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
 }
 
-foo()
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else{
+  alert('またチャレンジしてね');
+}
+
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand_name;
+
+  if (js_hand_num == 0){
+    hand_name = 'グー';
+  } else if (js_hand_num == 1){
+    hand_name = 'チョキ';
+  } else if (js_hand_num == 2){
+    hand_name = 'パー';
+  }
+
+  return hand_name;
+}
+
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
+
+// let fizzbuzz = function (num){
+
+//     if (num % 3 == 0){
+//       return 'fizz';
+//     } else if (num % 5 == 0){
+//       return 'buzz';
+//     } else if (num % 15 == 0){
+//       return 'fizzbuzz';
+//     } else {
+//       return num;
+//     }
+// }
+
+// for(let i = 1; i <= 30; i++){
+//   console.log(fizzbuzz(i));
+// }
+
